5dbc4521501ac51352256d37bd5f828241204a7d
Kamran Raza <137030916+KamranRaza786@users.noreply.github.com>
2023-09-14 08:18:59 +0500

    edit

--- server.mjs
+++ server.mjs
@@ -255,7 +255,24 @@ app.use((req, res) => {
 })
 
 
-const port = process.env.PORT || 5001;
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// const port = process.env.PORT || 5001;
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// // Serve React app's build files
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// // All other routes should point to the React app
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
